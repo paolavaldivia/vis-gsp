dd5d5899b69a19ccef8e401cc410589833c35b57
paoyo1 <paoyo1@gmail.com>
2015-12-11 23:04:12 -0200

    Initial structure

---
+++ config.py
@@ -0,0 +1,9 @@
+# Statement for enabling the development environment
+DEBUG = True
+
+# Define the application directory
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+
+# Secret key for signing cookies
+SECRET_KEY = "this-es-secredo"
